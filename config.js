f390fbbff802ee2586941b44b8e2296d7d2493b8
Nguyễn Đăng Khoa <neverfall109@gmail.com>
2022-06-15 20:55:43 +0700

    Release ttcode2

---
+++ config.js
@@ -0,0 +1,17 @@
+const CONFIG = {
+    introTitle: 'Diễm à!',
+    introDesc: `Dù Diễm có biến thành con gián 
+	thì vẫn iu Diễm v~ :v`,
+    btnIntro: 'hihi',
+    title: 'Phải chăng Diễm đã yêu tui ngay từ cái nhìn đầu tiên 😙',
+    desc: '',
+    btnYes: 'Đúng òiiii :3',
+    btnNo: 'Không nhaaaaaa',
+    question:'Viết lí do Diễm thích tui ở đây :v',
+    btnReply: 'Send',
+    reply: 'Như Khoa ai chả yêu :v xứng đáng có 10 ngy luôn chớ lị',
+    mess: 'Biết mà 🥰',
+    messDesc: '',
+    btnAccept: ' <3',
+    messLink: 'https://www.facebook.com/messages/t/100012582878440/' 
+}
